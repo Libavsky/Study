d71f258cb53c95289fd515d5180e6ba84957c26b
Libavsky <32072655+Libavsky@users.noreply.github.com>
2018-03-12 03:23:47 +0100

    aaa

--- signals/one.py
+++ signals/one.py
@@ -1,1 +1,13 @@
+import numpy as np
+import random
 print("lol")
+
+def szum_o_rozkladzie_jednostajnym(amplituda,t1,dlugosc,czestotliwosc = 1000.0):
+    random.seed()
+    fff = np.empty(int(dlugosc*czestotliwosc))
+    for x in range(len(fff)):
+        fff[x]=random.uniform(-amplituda,amplituda)
+    print(fff)
+
+
+szum_o_rozkladzie_jednostajnym(1,2.0,0.01)
